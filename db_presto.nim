d874c9ec5ed0f50561b32330a1e7022e656840a3
Bennyelg <Benjaminelgazar@gmail.com>
2017-09-16 14:15:53 +0300

    r

--- db_presto.nim
+++ db_presto.nim
@@ -1,9 +1,11 @@
+{.experimental.}
 import tables
 import strutils
 import sequtils
 import httpclient
 import json
 import os
+import db_common
 
 #[
     PrestoDb Connector
@@ -11,193 +13,224 @@ import os
     description: Simple Naive implementation in-order to use presto-db database.
 ]#
 
-proc range(size: int): seq[int] = 
-    result.newSeq(size)
-
 type 
     NoConnectionError = object of Exception
     CursorError = object of Exception
     QueryExecutionError = object of Exception
-    NoTransactionError = object of Exception
     NotValidProtocolError = object of Exception
+    
+    SqlQuery = distinct string
 
-type
     ResultSet = ref object
         client: HttpClient
         nextUri: string
         state: string
         columns: seq[string]
         data: seq[seq[string]]
-
+  
     Cursor = ref object
         catalog: string
         schema: string
         source: string
         sessionProps: string
-        poolInterval: int
+        pollInterval: int
         username: string
         tableCursor: bool
         host: string
         protocol: string
         port: string
         resultSet: ResultSet
-
+    
     Connection = ref object of RootObj
         host: string
         port: int
-        timeOutInSeconds: int
+        timeout: int
         cur: Cursor
 
-proc ping(this: Connection): bool =
-    var client = newHttpClient(timeout=this.timeOutInSeconds * 100)
-    var url = this.cur.protocol & "://" & this.host & ":" & this.port.intToStr
-    if client.get(url).status != Http200:
-        return false
-    return true
+using
+    cur: Cursor
+    con: Connection
 
-proc close*(this: Connection)  =
-    # There is no Actual close so just cleaning the connection information. 
-    this.cur = Cursor()
-    this.host = ""
-    this.port = -1
+proc ping(con): bool =
+    let client = newHttpClient(timeout = con.timeout * 100)
+    let url = "$1://$2:$3" % [con.cur.protocol, con.host, $con.port]
+    result = client.get(url).status == Http200
 
-proc commit*(this: Connection)  =
-    raise newException(NoTransactionError, "Presto does not have transcations.")
+proc close*(con) =
+    # There is no actual close so just cleaning the connection information. 
+    con.cur = Cursor()
+    con.host = ""
+    con.port = -1
 
-proc cursor*(this: Connection): Cursor  =
-    return this.cur
 
-proc rollback*(this: Connection)  =
-    raise newException(NoTransactionError, "Presto does not have transcations.")
 
-proc getColumns*(this: Cursor): seq[string] =
-    return this.resultSet.columns
+#proc commit*(con) {.error: "Presto doesn't have transaction support".}
 
-proc processResponse(this: Cursor, response: Response)  =
+#proc rollback*(con) {.error: "Presto doesn't have transaction support".}
+
+proc dbQuote*(s: string): string =
+  ## DB quotes the string.
+  result = "'"
+  for c in items(s):
+    if c == '\'': add(result, "''")
+    if c == '\\': add(result, "\\\\")
+    else: add(result, c)
+  add(result, '\'')
+
+proc dbFormat(formatstr: SqlQuery, args: varargs[string]): string =
+  result = ""
+  var a = 0
+  for c in items(string(formatstr)):
+    if c == '?':
+      if args[a] == nil:
+        add(result, "NULL")
+      else:
+        add(result, dbQuote(args[a]))
+      inc(a)
+    else:
+      add(result, c)
+
+
+template sql(query: string): SqlQuery = SqlQuery(query)
+
+template cursor*(con; useTableCur: bool = false): Cursor =
+    con.cur.tableCursor = if useTableCur: true else: false
+    con.cur
+
+template getColumns*(cur): seq[string] = cur.resultSet.columns
+
+proc processResponse(cur; response: Response) =
     if response.status != Http200:
-        raise newException(NoConnectionError, "Status code returned bad. $1" % response.status)
+        raise newException(NoConnectionError, "Bad response status code: $1" % response.status)
+    
     let data = parseJson(response.body)
+    
     if data.hasKey("error"):
-        raise newException(QueryExecutionError, data["error"]["message"].getStr)
-    var state = data["stats"]["state"].str
+        raise newException(QueryExecutionError, data["error"]["message"].str)
+    
+    let state = data["stats"]["state"].str
     if not data.hasKey("nextUri"):
-        this.resultSet.nextUri = ""
-        this.resultSet.state = "FINISHED"
+        cur.resultSet.nextUri = ""
+        cur.resultSet.state = "FINISHED"
         return
+    
     if not data.hasKey("columns"):
-        this.resultSet.nextUri = data["nextUri"].getStr
-        this.resultSet.state = state
-        this.resultSet.data = @[]
-        this.resultSet.columns = @[]
+        cur.resultSet.nextUri = data["nextUri"].getStr
+        cur.resultSet.state = state
+        cur.resultSet.data = @[]
+        cur.resultSet.columns = @[]
         return
+    
     if data.hasKey("data"):
-        if data.hasKey("nextUri"):
-            var nextUri = data["nextUri"].getStr
-            this.resultSet.nextUri = nextUri
-        else:
-            this.resultSet.nextUri = ""
-        var columns = data["columns"].mapIt(it["name"].str)
-        var dataset = data["data"].getElems
-        this.resultSet.data = @[]
-        for data in dataset:
-            this.resultSet.data.add(data.mapIt(it.getStr))
-        this.resultSet.state = state
-        this.resultSet.columns = columns
+        cur.resultSet.nextUri = data["nextUri"].getStr
+        let columns = data["columns"].mapIt(it["name"].str)
+        let dataset = data["data"].getElems
+        cur.resultSet.data = newSeq[seq[string]](dataset.len)
+        for i in 0..dataset.len - 1:
+            cur.resultSet.data[i] = dataset[i].mapIt(it.getStr)
+        cur.resultSet.state = state
+        cur.resultSet.columns = columns
+    
     if data.hasKey("nextUri") and not data.hasKey("data"):
-        var nextUri = data["nextUri"].getStr
-        this.resultSet.nextUri = nextUri
-        this.resultSet.state = state
-    return
+        cur.resultSet.nextUri = data["nextUri"].getStr
+        cur.resultSet.state = state
 
-proc execute*(this: Cursor, query: string)  =
+proc execute*(cur; query: SqlQuery) =
     var additional: seq[string] = @[]
-    if this.sessionProps.len != 0:
-        var additionalProperties = this.sessionProps.split(",")
-        var k = range(additionalProperties.len)
-        var evens = filter(zip(k, additionalProperties), proc (x: (int, string)): bool = x[0] mod 2 == 0)
-        var odds = filter(zip(k, additionalProperties), proc (x: (int, string)): bool = x[0] mod 2 != 0)
-        for ind in countup(0, evens.len - 1):
+    if cur.sessionProps.len != 0:
+        let additionalProperties = cur.sessionProps.split(",")
+        let k = newSeq[int](additionalProperties.len)
+        let evens = filterIt(zip(k, additionalProperties), it[0] mod 2 == 0)
+        let odds = filterIt(zip(k, additionalProperties), it[0] mod 2 != 0)
+        for ind in 0..<evens.len:
             additional.add(format("$1=$2", evens[ind][1], odds[ind][1]))
-    var client = newHttpClient()
-    var url = this.protocol & "://" & this.host & format(":$1", this.port) & "/v1/statement/"
-    client.headers = newHttpHeaders()
-    client.headers.add("X-Presto-Catalog", this.catalog)
-    client.headers.add("X-Presto-Schema", this.schema)
-    client.headers.add("X-Presto-Source", this.source)
-    client.headers.add("X-Presto-User", this.username)
+      
+    let client = newHttpClient()
+    let url = "$1://$2:$3/v1/statement" % [cur.protocol, cur.host, cur.port]
+    echo(url)
+    client.headers = newHttpHeaders(
+      {
+          "X-Presto-Catalog": cur.catalog,
+          "X-Presto-Schema": cur.schema,
+          "X-Presto-Source": cur.source,
+          "X-Presto-User": cur.username
+      }
+    )
     if additional.len > 0:
         client.headers.add("X-Presto-Session", additional.join(","))
-    this.resultSet = ResultSet(nextUri: url, state: "STARTED", columns: @[], client: client)
-    var response = client.request(this.resultSet.nextUri, httpMethod = HttpPost, body = query)
-    this.processResponse(response)
+    cur.resultSet = ResultSet(nextUri: url, state: "STARTED", columns: @[], client: client)
+    let response = client.request(cur.resultSet.nextUri, httpMethod = HttpPost, body = dbFormat(query))
+    cur.processResponse(response)
 
-proc fetchOne*(this: Cursor): seq[string] =
-    if this.resultSet.state == "FINISHED":
-        raise newException(CursorError, "No More Rows.")
+proc fetchOne*(cur): seq[string] =
+    if cur.resultSet.state == "FINISHED":
+        raise newException(CursorError, "No more rows left.")
     
-    if this.resultSet.data.len > 0:
-        return this.resultSet.data.pop()
-    elif this.resultSet.data.len == 0:
-        os.sleep(this.poolInterval)
-        var response = this.resultSet.client.request(this.resultSet.nextUri, httpMethod = HttpGet)
-        this.processResponse(response)
+    if cur.resultSet.data.len > 0:
+        return cur.resultSet.data.pop()
+    
+    elif cur.resultSet.data.len == 0:
+        os.sleep(cur.pollInterval)
+        var response = cur.resultSet.client.request(cur.resultSet.nextUri, httpMethod = HttpGet)
+        cur.processResponse(response)
+    try:
+        result = cur.resultSet.data.pop()
+    except IndexError:
+        raise newException(CursorError, "No more rows left.")
 
-    return this.resultSet.data.pop()
-
-proc fetchMany*(this: Cursor, amount: int): seq[seq[string]]  =
-    var setOfResults: seq[seq[string]] = @[]
-    for _ in countup(1, amount):
-        var element = this.fetchOne()
-        setOfResults.add(element)
-    return setOfResults
-
-proc fetchAll*(this: Cursor): seq[seq[string]] =
-    var queryData: seq[seq[string]] = @[]
-    while true:
-        if this.resultSet.state == "FINISHED":
+proc fetchMany*(cur; amount: int): seq[seq[string]] =
+    var dataSet: seq[seq[string]] = @[]
+    for i in 0..<amount:
+        try:
+            var row = cur.fetchOne()
+            dataSet.add(row)
+        except CursorError:
             break
-        if this.resultSet.data.len > 0:
-            queryData.add(this.resultSet.data)
-        os.sleep(this.poolInterval)
-        var response = this.resultSet.client.request(this.resultSet.nextUri, httpMethod = HttpGet)
-        this.processResponse(response)
-    return queryData
+    return dataSet
+     
 
-proc open*(host: string, port: int, protocol: string = "http", catalog: string, schema: string,
-           username: string, source = "NimPresto", poolInterval = 1,
-           sessionProps = "", tableCursor = false): Connection =
+proc fetchAll*(cur): seq[seq[string]] =
+    result = @[]
+    while cur.resultSet.state != "FINISHED":
+        if cur.resultSet.data.len > 0:
+            result.add(cur.resultSet.data)
+        os.sleep(cur.pollInterval)
+        let response = cur.resultSet.client.request(cur.resultSet.nextUri, httpMethod = HttpGet)
+        cur.processResponse(response)
+
+proc open*(host: string, port: int, protocol = "http",
+           catalog, schema, username: string, source = "NimPresto",
+           pollInterval = 1, sessionProps = "", tableCursor = false): Connection =
+    
     if protocol notin ["http", "https"]:
-        raise newException(NotValidProtocolError, "The protocol you specified is not valid protocol: $1" % protocol)
-    let cursor = Cursor(catalog: catalog,
-                        schema: schema,
-                        source: source, 
-                        sessionProps: sessionProps,
-                        poolInterval: poolInterval * 1000,
-                        tableCursor: tableCursor,
-                        username: username,
-                        host: host,
-                        protocol: protocol,
-                        port: port.intToStr)
-    let connection = Connection(
+        raise newException(NotValidProtocolError, "Not valid protocol: $1" % protocol)
+    
+    let cursor = Cursor(
+      catalog: catalog,
+      schema: schema,
+      source: source, 
+      sessionProps: sessionProps,
+      pollInterval: pollInterval * 1000,
+      username: username,
+      host: host,
+      protocol: protocol,
+      port: $port
+    )
+
+    result = Connection(
         host: host,
         port: port,
-        timeOutInSeconds: 10,
+        timeout: 5,
         cur: cursor
     )
-    if not connection.ping():
-        raise newException(NoConnectionError, "Failed to Established Connection.")
-    return connection
+
+    if not result.ping():
+        raise newException(NoConnectionError, "Failed to connect to the database.")
 
 
 when isMainModule:
-    let con = open(host="host.com", protocol="xxx", port=8889, catalog="hive", schema="dwh", username="benny")
+    let con = open(host="xxxx", port=8889, catalog="hive", schema="dwh", username="benny")
     defer: con.close()
     var cur = con.cursor()
-    cur.execute("SELECT * FROM abc")
-    echo(cur.fetchOne())
-    #echo(cur.getColumns())
-    # for s in cur.fetchMany(10):
-    #     echo(s)
-    # echo(cur.fetchOne())
-    # echo("===")
-    # echo(cur.fetchOne())
+    cur.execute(sql"SELECT * FROM table LIMIT 10")
+    echo(cur.fetchMany(10))
