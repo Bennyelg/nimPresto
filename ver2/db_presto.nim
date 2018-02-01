2d3c54cf4c9f16f4ea7610771dc1ddd9a0286a79
bennyelg <benjaminelgazar@gmail.com>
2018-02-01 22:44:34 +0200

    more clean & changes.

--- ver2/db_presto.nim
+++ ver2/db_presto.nim
@@ -3,52 +3,59 @@ import httpclient, base64, json, strutils, sequtils
 type
   
   PrestoClient = ref object of RootObj
-    client: HttpClient
-    url, port: string
-    status: Status
-    timeout: int
-    columns: seq[string]
+    client:    HttpClient
+    url:       string
+    port:      string
+    status:    Status
+    timeout:   int
   
   SqlQuery = distinct string
   
   Status {.pure.} = enum
-    finished = "FINISHED", started = "STARTED", planning = "PLANNING"
-
-
-proc newPrestoClient* (protocol, host, port, 
-                       catalog, schema: string): PrestoClient =
-  let client = newHttpClient()
-  client.headers = newHttpHeaders({
-    "X-Presto-Catalog": catalog,
-    "X-Presto-Schema":  schema,
-    "X-Presto-Source": "nimPersto",
-    "X-Presto-User": "nimPresto"
-  })
-  return PrestoClient(
-    client: client,
-    url: "$1://$2:$3/v1/statement" % [protocol, host, port],
-    timeout: 100,
-    status: Status.started
-  )
+    finished  = "FINISHED",
+    started   = "STARTED", 
+    planning  = "PLANNING"
 
 using
   self: PrestoClient
 
-proc newPrestoClient* (protocol, host, port, catalog,
-                       schema, username, password: string): PrestoClient =
+proc newPrestoClient* (protocol, host, port, 
+                       catalog, schema: string): PrestoClient =
+  
   let client = newHttpClient()
+  
   client.headers = newHttpHeaders({
     "X-Presto-Catalog": catalog,
     "X-Presto-Schema":  schema,
-    "X-Presto-Source": "nimPersto",
-    "X-Presto-User": username,
-    "Authorization": "Basic " & encode(username & ":" & password)
+    "X-Presto-Source":  "nimPersto",
+    "X-Presto-User":    "nimPresto"
   })
+
   return PrestoClient(
-    client: client,
-    url: "$1://$2:$3/v1/statement" % [protocol, host, port],
+    client:  client,
+    url:     "$1://$2:$3/v1/statement" % [protocol, host, port],
     timeout: 100,
-    status: Status.started
+    status:  Status.started
+  )
+
+proc newPrestoClient* (protocol, host, port, catalog,
+                       schema, username, password: string): PrestoClient =
+  
+  let client = newHttpClient()
+  
+  client.headers = newHttpHeaders({
+    "X-Presto-Catalog": catalog,
+    "X-Presto-Schema":  schema,
+    "X-Presto-Source":  "nimPersto",
+    "X-Presto-User":    username,
+    "Authorization":    "Basic " & encode(username & ":" & password)
+  })
+  
+  return PrestoClient(
+    client:  client,
+    url:     "$1://$2:$3/v1/statement" % [protocol, host, port],
+    timeout: 100,
+    status:  Status.started
   )
 
 proc dbQuote(s: string): string =
@@ -73,46 +80,48 @@ proc dbFormat(formatstr: SqlQuery, args: varargs[string]): string =
     else:
       add(result, c)
 
-proc checkStatuses(self; data: JsonNode) =
+proc syncProgress(self; data: JsonNode) =
 
   let state = data["stats"]["state"].str
+  
   if state == "FINISHED" or not data.hasKey("nextUri"):
     self.status = Status.finished
   
-  if data.hasKey("nextUri") and not data.hasKey("data"):
+  if data.hasKey("nextUri"):
     self.url = data["nextUri"].getStr
   
-  if not data.hasKey("columns"):
-    self.url = data["nextUri"].getStr
-  
+proc extractQueryData(responseDataChunk: JsonNode): seq[JsonNode] =
+  responseDataChunk.getElems
 
 iterator execute(self; query: SqlQuery): JsonNode =
-  var response = self.client.request(self.url, httpMethod = HttpPost, body = dbFormat(query))
-  let respData = parseJson(response.body)
-  self.checkStatuses(respData)
+  
   var i = 0
+  var response = self.client.request(self.url, httpMethod = HttpPost, body = dbFormat(query))
+  var respData = parseJson(response.body)
+  
+  self.syncProgress(respData)
+  
   while self.status != Status.finished:
+    
     if i != 0:
       response = self.client.request(self.url, httpMethod = HttpGet, body = dbFormat(query))
+      respData = parseJson(response.body)
+    
     i += 1
-    let respData = parseJson(response.body)
-    self.checkStatuses(respData)
+    
     if respData.hasKey("data"):
-      if self.columns.len == 0:
-        self.columns = respData["columns"].mapIt(it["name"].str)
-      let dataset = respData["data"].getElems
-      echo(dataset.len)
-      for i in 0..dataset.len - 1:
-        yield dataset[i]
+      for row in extractQueryData(respData["data"]):
+        yield row
 
-    self.checkStatuses(respData)
+      #   self.columns = respData["columns"].mapIt(it["name"].str)
 
-
+    self.syncProgress(respData)
 
 
 when isMainModule:
-  let prestoConnection = newPrestoClient("https", "host", "port", "hive", "default", "user", "password")
-  for row in prestoConnection.execute(SqlQuery("SELECT * from default.live_events limit 5")):
-    echo(row)
+  let prestoConnection = newPrestoClient("https", "host", "8443", "hive", "default", "user", "pass")
+  for r in prestoConnection.execute(SqlQuery("SELECT * from default.live_events limit 5")):
+    echo(r)
+  
 
 
